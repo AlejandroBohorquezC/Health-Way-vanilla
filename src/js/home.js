0a492d9e356564f670d79ce1972000eadce9ef42
Alejo Bohórquez <therosseanux@gmail.com>
2023-05-22 11:08:23 -0500

    mejora en el llamado a la api, cambios en el mapeo hijos, y redireccionamiento a una nueva vista

--- src/js/home.js
+++ src/js/home.js
@@ -1,21 +1,23 @@
 import axios  from "axios";
+import { APP_ID, APP_KEY } from "../../config";
 
+
+const baseUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
 const form = document.querySelector('.form');
-
-const baseUrl = 'https://api.edamam.com/api/recipes/v2?type=public&app_id=4fdaa3a2&app_key=09dfc090c8477e00c5957e11c829b34b';
+const inputText = document.getElementById('input-field');
 
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
+    e.preventDefault();
     
-    const inputText = document.getElementById('input-field').value;
+    const recipe = inputText.value;
 
-    if (inputText.trim() === '') {
-        alert('Por favor, ingresa un valor válido.');
-        return; // Detener la ejecución del evento submit si el campo está vacío
+    if (recipe <= 0) {
+        return alert('Por favor, ingresa un nombre de comida válido.');
     };
-    searchFood(inputText);
+
+    searchFood(recipe);
 });
 
 const searchFood = async (comida) => {
@@ -28,13 +30,38 @@ const searchFood = async (comida) => {
     };
     const {hits} = data;
     const list = document.querySelector('.container');
-    const recipes = hits.map(({recipe}) => 
-    (`
-        <div key="${recipe.label}" class="recipe">    
-            <p class="recipe-name">${recipe.label}</p>
-            <img class="recipe-image" src='${recipe.image}' />
-        </div>
-    `))
+    
+    inputText.value = '';
+    list.innerHTML = '';
 
-    list.innerHTML = recipes;
+    const recipes = hits.map(({ recipe }) => {
+        const recipeElement = document.createElement('div');
+        recipeElement.classList.add('recipe');
+        
+        const recipeNameElement = document.createElement('p');
+        recipeNameElement.classList.add('recipe-name');
+        recipeNameElement.textContent = recipe.label;
+        
+        const recipeImageElement = document.createElement('img');
+        recipeImageElement.classList.add('recipe-image');
+        recipeImageElement.src = recipe.image;
+        
+        
+        recipeElement.addEventListener('click', () => {
+            navigateToRecipeDetails(recipe);
+        });
+        
+        recipeElement.appendChild(recipeNameElement);
+        recipeElement.appendChild(recipeImageElement);
+        return recipeElement;
+    });
+    
+    list.append(...recipes);
 };
+
+const navigateToRecipeDetails = (recipe) => {
+    // const recipeLS = JSON.stringify(recipe);
+    // localStorage.setItem('recipe', recipeLS);
+    window.location.href = `recipe.html`;
+    console.log(recipe);
+}
